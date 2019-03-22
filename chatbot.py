5a95f126bf3781d96dbdbcf586b718ea1e544602
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2019-03-22 18:10:11 +0100

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,23 +1,19 @@
-import time
+from chatterbot import ChatBot
 import win32com.client as wc
-import random
-from conversaciones import despedidas
-#CHAT-BOT
+#from chatterbot.trainers import ChatterBotCorpusTrainer 
+#from chatterbot.trainers import ListTrainer 
+#chatbot = ChatBot( "Ejemplo de bot" ) 
+#trainer = ChatterBotCorpusTrainer(chatbot) 
+#trainer.train( "chatterbot.corpus.spanish" )
 speak=wc.Dispatch("Sapi.SpVoice")
+chatbot = ChatBot(
 
-a=input("Escribe algo: ")
-momento=time.localtime()
-if momento[3]>=12 and momento[3]<22:
-    print("GOOD AFTERNOON");speak.Speak("good afternoon")
-elif momento[3]>=1 and momento[3]<12:
-    print("GOOD MORNING");speak.Speak("good morning")
-elif momento[3]>=22 and momento[3]<=23:
-    print("GOOD NIGHT");speak.Speak("good night")
-texto=("")
-while texto!=("see you") and texto!=("bye bye") and texto!=("good bye"):
-    texto=input("Texto: ")
-    texto=texto.lower()
+    "Ejemplo Bot",
+    trainer = "chatterbot.trainers.ChatterBotCorpusTrainer"
+    )
 
-
-despedida=random.choice(despedidas)
-speak.Speak(despedida)
+while True:
+    usuario = input(">>> ")
+    respuesta = chatbot.get_response(usuario)
+    speak.Speak(respuesta)
+    print("BOT: "+str(respuesta))
